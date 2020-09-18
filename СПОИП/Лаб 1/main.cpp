bd85982d1ab0fab44ca375a824b2066b36760d03
hacknazar21 <nazarcool2001@gmail.com>
2020-09-18 15:04:11 +0500

    Рабочий вариант

--- СПОИП/Лаб 1/main.cpp
+++ СПОИП/Лаб 1/main.cpp
@@ -14,13 +14,16 @@
 *** n - позиция
 *** s - подстрока
 *** ch - char
-*** cc - counted chars
+*** i, itter, cc - иттераторы для пробегания по строке/ам
+*** code_error - Код ошибки для вывода соответствующего сообщения 
+*** const_string - Исходная строка
+*** edited_string - Измененная строка
 
 */
 #define N 256
 
-char const_string[13] = "Hello World!", edited_string[13], ch;
-int i, cc = 0;
+char const_string[N] = "Supernova will be the first", edited_string[N], ch;
+int i, cc = 0, itter = 0;
 long int code_error;
 
 #include <iostream>
@@ -29,29 +32,50 @@ void Error(long int err_code)
 {
 	switch(err_code)
 	{
-		case 100:
+		case 0:
+		std::cout << "Error 0 - Symbols doesn't match!\n"
+				  << "Please exit and try again";
+		break;
+		case 1:
+		std::cout << "Error 1 - Position is out of range, maybe you entered too long word!\n"
+				  << "Please exit and try again";
+		break;
+		case 2:
+		std::cout << "Error 2- The word longer than line, or position entered incorrect!\n"
+				  << "Please exit and try again";
 		break;
 	}
 }
 
 bool isAllCorrect(char* s, int n, int l)
 {
-	ch = s[n-1];
+	ch = s[itter];
 	i = n-1;
 	while(ch != '\0') // Пока позиция не в конце строки
 	{
 		if(
-			i < n+l // Позиция за пределами заданной границы
+			i < n+l // Находится в пределах заданных границ
 		   )
 		{
 			if(
 				ch != const_string[i++] // Символы не совпадают
-				) 
-				return false;
-			ch = s[i];
+				)
+			{
+				code_error = 0;
+				return false; // Ошибка 0
+			}
+			if(itter < l) ch = s[++itter];
 		}
-		else return false;
-		if(const_string[i] == '\0' && ch != 0) return false;
+		else // Позиция за пределами заданной границы
+		{
+			code_error = 1;
+			return false; // Ошибка 1
+		}
+		if(const_string[i] == '\0' && ch != 0) // Слово длиннее строки, либо позиция указана не верно 
+		{
+			code_error = 2;
+			return false; // Ошибка 2
+		}
 	}
 	return true;
 }
@@ -63,15 +87,14 @@ char* Delete(char* s, int n, int l)
 		Error(code_error); // Если есть ошибки, то выводим его пользователю
 		return "\0"; // Возращаем нулевой байт в качестве указания, что что-то пошло не так
 	}
-	i = n-1;
-	ch = const_string[i];
-	while(ch != '\0') 
+	i = 0; // Обнуление для пробегания по исходной строке
+	while(ch != '\0') // Пока позиция не в конце строки
 	{
-		if (i < n + l - 1 && i >= n-1) i++;
+		if (i < n + l - 1 && i >= n-1) i++; // Если позиция в удаляемой	зоне, то накручиваем i, как бы пропуская этот промежуток
 		else 
 		{
-			ch = const_string[i++];
-			edited_string[cc++] = ch;
+			ch = const_string[i++]; // Бежим по строке и берем символы
+			edited_string[cc++] = ch; // Записываем символы в новую измененную строку
 		}
 	}
 	return edited_string;
@@ -81,11 +104,20 @@ char* Delete(char* s, int n, int l)
 int main(int argc, char const *argv[])
 {
 
-	char L[13] = "Hello";
+	char L[N];
+	int pos, len;
 
 	std::cout << "NO Edited: " << const_string << "\n";
-	std::cout << "Edited: " << Delete(L, 1, 5) << "\n";
 
-	std::cin.get();
+	std::cout << "Enter substring: " << "\n";
+	std::cin.getline(L, N);
+	std::cout << "Enter position number: " << "\n";
+	std::cin >> pos;
+	std::cout << "Enter the length: " << "\n";
+	std::cin >> len;
+
+	
+	std::cout << "Edited: " << Delete(L, pos, len) << "\n";
+	std::cin.getline(L, N);
 	return 0;
 }
